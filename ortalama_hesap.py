a234221586a846df4c26f90987e48ff2493600eb
salimunlu <142517512+salimunlu@users.noreply.github.com>
2023-11-24 16:46:41 +0300

    ortalama_hesap dosyası eklendi

---
+++ ortalama_hesap.py
@@ -0,0 +1,8 @@
+# Kullanıcıdan 2 adet not alınacak
+mat = float(input("Matematik notunuzu giriniz: "))  # matematik notu
+ing = float(input("İngilizce notunuzu giriniz: "))  # ingilizce notu
+
+ortalama = (mat + ing) / 2    # ortalama hesabı
+
+# Kullanıcıya not ortalaması gösterilecek
+print(f"Not ortalamanız {ortalama}'dır.")
